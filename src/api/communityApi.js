f2f14d8c423978bd429a9feef0ff1dcee6330e78
Sukumar.B <sukumar200508@gmail.com>
2026-03-01 21:37:34 +0530

    Deleted

--- src/api/communityApi.js
+++
@@ -1,9 +0,0 @@
-import { appClient } from "@/api/appClient";
-
-export async function fetchPosts() {
-  return appClient.entities.ForumPost.list("-created_date");
-}
-
-export async function createPost(postData) {
-  return appClient.entities.ForumPost.create(postData);
-}
